ed2fd33b002709d545c1e4be02e227f0f74054a8
Vital Yurau <vialyur@list.ru>
2019-03-24 20:40:32 +0300

    feat: try 2

--- src/index.js
+++ src/index.js
@@ -1,18 +1,51 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  let openingBracket = ['(', '{', '[', '|', '1', '5', '7', '8'];
-  let closingBracket = [')', '}', ']', '|', '2', '6', '7', '8'];
+  let openingBracket = [];
+  let closingBracket = [];
+  let sameBrackets = [];
 
-    if (check.lenght > 0){
-
-      for (let i = 0; i < check.length; i += 1) {
-  
-        if (openingBracket.indexOf(check[i]) == closingBracket.IndexOf(check[(i + 1)])) {
-          check.splice(i, 2);
-        }
-      }
-
-    } else {      
-      return true;
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let box = bracketsConfig[i];
+    if (box[0] != box[1]) {
+      openingBracket.push(box[0]);
+      closingBracket.push(box[1]);
+    } else {
+      sameBrackets.push(box[0]);
     }
+  }
+
+  if (!str.length) {
+    return false;
+  }
+
+  let stash = [];
+  for (let i = 0; i < str.length; i++) {
+    if (openingBracket.indexOf(str[i]) >= 0) {
+      stash.push(str[i]);
+    } else if (sameBrackets.indexOf(str[i]) >= 0) {
+      let last = stash[stash.length - 1];
+      if (sameBrackets.indexOf(last) >= 0) {
+        if (sameBrackets.indexOf(last) == sameBrackets.indexOf(str[i])) {
+          stash.pop();
+        } else {
+          stash.push(str[i]);
+        }  
+      } else {
+        stash.push(str[i]);
+      }
+    } else if (closingBracket.indexOf(str[i]) >= 0) {
+      let last = stash[stash.length - 1];
+      if (openingBracket.indexOf(last) == closingBracket.indexOf(str[i])) {
+        stash.pop();
+      } else {
+        return false;        
+      }
+    }
+  }
+
+  if (stash.length) {
+    return false;
+  } else {
+  return true;
+  }
 }
