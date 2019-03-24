4334bc4aaff4046d4b2a16022c0837ba8d1b465a
Vital Yurau <vialyur@list.ru>
2019-03-24 11:00:26 +0300

    feat: first attempt at creating

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let openingBracket = ['(', '{', '[', '|', '1', '5', '7', '8'];
+  let closingBracket = [')', '}', ']', '|', '2', '6', '7', '8'];
+
+    if (check.lenght > 0){
+
+      for (let i = 0; i < check.length; i += 1) {
+  
+        if (openingBracket.indexOf(check[i]) == closingBracket.IndexOf(check[(i + 1)])) {
+          check.splice(i, 2);
+        }
+      }
+
+    } else {      
+      return true;
+    }
 }
